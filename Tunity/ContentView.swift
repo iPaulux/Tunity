d9c71687eb6bd9106909dbae410587c5b933ce6b
Paul <ipauluxyt@gmail.com>
2024-01-16 21:24:10 +0100

    Initial Commit

---
+++ Tunity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tunity
+//
+//  Created by Paul Guttin on 16/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
